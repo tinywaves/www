d58c73d42f82ec6ead25121293b4b4a2f960101a
Donghui Zheng <zhengdonghui@magiccreative.tech>
2024-01-24 14:20:52 +0800

    chore: change desc and social list.

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,11 +4,11 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Easton Zheng",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "My Personal Website",
+	title: "Personal Website",
 	// Meta property used as the default description meta property
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "en",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
