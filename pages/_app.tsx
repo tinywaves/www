1baff9305a5860dfa41d7660879478393acc5c3b
tinywaves <dhzhme@gmail.com>
2024-04-18 18:02:46 +0800

    feat: new structure.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '#/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Manrope } from 'next/font/google';
+
+const manrope = Manrope({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={manrope.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
