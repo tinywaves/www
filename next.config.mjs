067062b053616d0bd2f412f2b10fb77f0b5c9756
tinywaves <dhzhme@gmail.com>
2024-04-19 00:42:22 +0800

    chore: open copy button.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import nextra from 'nextra';
 const withNextra = nextra({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
 });
 
 /** @type {import('next').NextConfig} */
