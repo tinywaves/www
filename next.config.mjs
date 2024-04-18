271cce2fd4b971eb135fb24ce431ec0b5f480ea8
tinywaves <dhzhme@gmail.com>
2024-04-18 15:09:47 +0800

    feat: add nextra.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import nextra from 'nextra';
+
+const withNextra = nextra({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.tsx',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
