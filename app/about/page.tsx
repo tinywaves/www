656f084b14a2c02edfe536beded65634d74767ef
tinywaves <dhzhme@gmail.com>
2024-04-25 00:01:02 +0800

    feat: update authors and about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,19 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from 'app/seo'
+import type { Authors } from 'contentlayer/generated';
+import { allAuthors } from 'contentlayer/generated';
+import { MDXLayoutRenderer } from 'pliny/mdx-components';
+import { coreContent } from 'pliny/utils/contentlayer';
+import { genPageMetadata } from 'app/seo';
+import AuthorLayout from '@/layouts/AuthorLayout';
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'About' });
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors;
+  const mainContent = coreContent(author);
 
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
-  )
+    <AuthorLayout content={mainContent}>
+      <MDXLayoutRenderer code={author.body.code} />
+    </AuthorLayout>
+  );
 }
