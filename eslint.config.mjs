e20e3626329cfe2f9e9ccb5b39f2005449b7521b
tinywaves <dhzhme@gmail.com>
2024-07-03 10:56:35 +0800

    k

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import dhzh from '@dhzh/eslint-config';
-
-export default dhzh({
-  formatters: true,
-  react: true,
-  rules: {
-    'react-refresh/only-export-components': 'off',
-  },
-});
--- types/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from 'react';
-
-export interface IProject {
-  title: string;
-  description: string;
-  href: string;
-  icon: ReactNode;
-}
