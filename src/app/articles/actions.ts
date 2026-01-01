6fc597571881be6ea860b160d2cfda779c174f6f
tinywaves <dhzhme@gmail.com>
2026-01-01 17:40:18 +0800

    feat: make getMoreArticles asynchronous to support await functionality

--- src/app/articles/actions.ts
+++ src/app/articles/actions.ts
@@ -2,7 +2,8 @@
 
 import { getAllArticles } from '#/lib/articles';
 
-export const getMoreArticles = (page: number, limit = 10) => {
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getMoreArticles = async (page: number, limit = 10) => {
   const allArticles = getAllArticles();
   const start = (page - 1) * limit;
   const end = start + limit;
